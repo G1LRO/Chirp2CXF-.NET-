7f4e8631b371447f0578de4e41d2fd13faf6a00e
G1LRO <133013430+G1LRO@users.noreply.github.com>
2023-05-12 16:24:14 +0100

    Add project files.

---
+++ Chirp2CXF(.NET)/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnOpenCSV = New Button()
+        lblHelloWorld = New Label()
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        OpenFileDialog1 = New OpenFileDialog()
+        OpenFileDialog2 = New OpenFileDialog()
+        btnOpenCXF = New Button()
+        lblCXFfileselected = New Label()
+        btnCreate = New Button()
+        lblResult = New Label()
+        ListBox1 = New ListBox()
+        RichTextBox1 = New RichTextBox()
+        SuspendLayout()
+        ' 
+        ' btnOpenCSV
+        ' 
+        btnOpenCSV.Location = New Point(62, 272)
+        btnOpenCSV.Name = "btnOpenCSV"
+        btnOpenCSV.Size = New Size(161, 29)
+        btnOpenCSV.TabIndex = 0
+        btnOpenCSV.Text = "Select CHIRP csv file"
+        btnOpenCSV.UseVisualStyleBackColor = True
+        ' 
+        ' lblHelloWorld
+        ' 
+        lblHelloWorld.AutoSize = True
+        lblHelloWorld.Location = New Point(66, 316)
+        lblHelloWorld.Name = "lblHelloWorld"
+        lblHelloWorld.Size = New Size(134, 20)
+        lblHelloWorld.TabIndex = 1
+        lblHelloWorld.Text = "no csv file selected"
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        OpenFileDialog1.Filter = "CSV Files (*.csv)|*.csv"
+        ' 
+        ' OpenFileDialog2
+        ' 
+        OpenFileDialog2.FileName = "OpenFileDialog2"
+        OpenFileDialog2.Filter = "CXF File (*.cxf)|*.cxf"
+        ' 
+        ' btnOpenCXF
+        ' 
+        btnOpenCXF.Location = New Point(66, 363)
+        btnOpenCXF.Name = "btnOpenCXF"
+        btnOpenCXF.Size = New Size(158, 29)
+        btnOpenCXF.TabIndex = 2
+        btnOpenCXF.Text = "Select CPS cxf file"
+        btnOpenCXF.UseVisualStyleBackColor = True
+        ' 
+        ' lblCXFfileselected
+        ' 
+        lblCXFfileselected.AutoSize = True
+        lblCXFfileselected.Location = New Point(62, 412)
+        lblCXFfileselected.Name = "lblCXFfileselected"
+        lblCXFfileselected.Size = New Size(133, 20)
+        lblCXFfileselected.TabIndex = 3
+        lblCXFfileselected.Text = "no cxf file selected"
+        ' 
+        ' btnCreate
+        ' 
+        btnCreate.Location = New Point(62, 462)
+        btnCreate.Name = "btnCreate"
+        btnCreate.Size = New Size(158, 29)
+        btnCreate.TabIndex = 4
+        btnCreate.Text = "Create new CPS file"
+        btnCreate.UseVisualStyleBackColor = True
+        ' 
+        ' lblResult
+        ' 
+        lblResult.AutoSize = True
+        lblResult.Location = New Point(62, 508)
+        lblResult.Name = "lblResult"
+        lblResult.Size = New Size(15, 20)
+        lblResult.TabIndex = 5
+        lblResult.Text = "-"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 20
+        ListBox1.Location = New Point(508, 19)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(266, 404)
+        ListBox1.TabIndex = 7
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(66, 19)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(397, 229)
+        RichTextBox1.TabIndex = 8
+        RichTextBox1.Text = resources.GetString("RichTextBox1.Text")
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(795, 550)
+        Controls.Add(RichTextBox1)
+        Controls.Add(ListBox1)
+        Controls.Add(lblResult)
+        Controls.Add(btnCreate)
+        Controls.Add(lblCXFfileselected)
+        Controls.Add(btnOpenCXF)
+        Controls.Add(lblHelloWorld)
+        Controls.Add(btnOpenCSV)
+        Name = "Form1"
+        Text = "CHIRP2cxf(.NET)"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnOpenCSV As Button
+    Friend WithEvents lblHelloWorld As Label
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+    Friend WithEvents btnOpenCXF As Button
+    Friend WithEvents lblCXFfileselected As Label
+    Friend WithEvents btnCreate As Button
+    Friend WithEvents lblResult As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ Chirp2CXF(.NET)/Form1.vb
@@ -0,0 +1,220 @@
+﻿Imports System.IO
+Imports System.Windows.Forms.LinkLabel
+Imports Microsoft.VisualBasic.FileIO
+
+Public Class Form1
+    Public chirpfile As String = " "
+    Public cxffile As String = " "
+    Public targetfile As String
+
+
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles btnOpenCSV.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            lblHelloWorld.Text = OpenFileDialog1.FileName
+            chirpfile = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnOpenCXF.Click
+        If OpenFileDialog2.ShowDialog() = DialogResult.OK Then
+            lblCXFfileselected.Text = OpenFileDialog2.FileName
+            cxffile = OpenFileDialog2.FileName
+        End If
+    End Sub
+
+
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+        targetfile = Path.GetDirectoryName(cxffile) & "\processed_" & Path.GetFileName(cxffile)
+
+        If chirpfile.Contains(".csv") And cxffile.Contains(".cxf") Then
+
+            Dim ctcss_tones() As Double = {
+            67.0, 69.3, 71.9, 74.4, 77.0, 79.7, 82.5, 85.4, 88.5, 91.5,
+            94.8, 97.4, 100.0, 103.5, 107.2, 110.9, 114.8, 118.8, 123.0, 127.3,
+            131.8, 136.5, 141.3, 146.2, 151.4, 156.7, 159.8, 162.2, 165.5, 167.9,
+            171.3, 173.8, 177.3, 179.9, 183.5, 186.2, 189.9, 192.8, 196.6, 199.5,
+            203.5, 206.5, 210.7, 218.1, 225.7, 229.1, 233.6, 241.8, 250.3
+            }
+            Using input_file As New StreamReader(cxffile)
+                Using output_file As New StreamWriter(targetfile)
+                    Dim line As String
+
+                    ' Read each line from the input file
+                    While Not input_file.EndOfStream
+                        line = input_file.ReadLine()
+
+                        ' Write the line to the output file
+                        output_file.WriteLine(line)
+
+                        ' Check if the line contains the target word
+                        If line.Contains("<Channels_MR>") Then
+                            ' If so, stop reading and writing
+                            Exit While
+                        End If
+                    End While
+                End Using
+            End Using
+
+            Using csv_file As New TextFieldParser(chirpfile)
+                csv_file.TextFieldType = FieldType.Delimited
+                csv_file.SetDelimiters(",")
+
+                ' Read the header row to get the field names
+                Dim fieldNames As String() = csv_file.ReadFields()
+
+                While Not csv_file.EndOfData
+                    ' Read each row of data
+                    Dim rowData As String() = csv_file.ReadFields()
+
+                    ' Put each value into a separate variable
+                    Dim Location As String = rowData(Array.IndexOf(fieldNames, "Location"))
+                    Dim Name As String = rowData(Array.IndexOf(fieldNames, "Name"))
+                    Dim Frequency As String = rowData(Array.IndexOf(fieldNames, "Frequency"))
+                    Dim Duplex As String = rowData(Array.IndexOf(fieldNames, "Duplex"))
+                    Dim Offset As String = rowData(Array.IndexOf(fieldNames, "Offset"))
+                    Dim Tone As String = rowData(Array.IndexOf(fieldNames, "Tone"))
+                    Dim rToneFreq As String = rowData(Array.IndexOf(fieldNames, "rToneFreq"))
+                    Dim cToneFreq As String = rowData(Array.IndexOf(fieldNames, "cToneFreq"))
+                    Dim DtcsCode As String = rowData(Array.IndexOf(fieldNames, "DtcsCode"))
+                    Dim DtcsPolarity As String = rowData(Array.IndexOf(fieldNames, "DtcsPolarity"))
+                    Dim RxDtcsCode As String = rowData(Array.IndexOf(fieldNames, "RxDtcsCode"))
+                    Dim CrossMode As String = rowData(Array.IndexOf(fieldNames, "CrossMode"))
+                    Dim TStep As String = rowData(Array.IndexOf(fieldNames, "TStep"))
+                    Dim Skip As String = rowData(Array.IndexOf(fieldNames, "Skip"))
+                    Dim Power As String = rowData(Array.IndexOf(fieldNames, "Power"))
+                    Dim Mode As String = rowData(Array.IndexOf(fieldNames, "Mode"))
+
+                    ' Process the row data
+                    ' Check location is ok (<200)
+                    If Convert.ToDouble(Location) > 200 Then
+                        Exit While
+                    End If
+                    Dim chanIndex As String = Location
+
+                    ' Name goes into <Name>
+                    Name = Name.Substring(0, Math.Min(Name.Length, 10))
+
+                    ' Tidy up for <Rxfreq>
+                    Dim RxFreq As Double = Convert.ToDouble(Frequency)
+                    RxFreq = Math.Round(RxFreq, 6)
+
+                    ' Calculate Transmit <Txfreq> from Receive and Offset Xml variable <Txfreq>
+                    Dim TxFreq As Double = Convert.ToDouble(Frequency) + Convert.ToDouble(Offset) ' CHIRP sets Offset to 0 if not used
+                    If Duplex = "-" Then
+                        TxFreq = Convert.ToDouble(Frequency) - Convert.ToDouble(Offset)
+                    End If
+                    TxFreq = Math.Round(TxFreq, 6)
+
+                    ' Calculate ctcss tone index position for rx and tx tones
+                    Dim AnaTxCTCIndex As Integer = 1
+                    Dim AnaRxCTCIndex As Integer = 1
+
+
+                    If ctcss_tones.Contains(Double.Parse(rToneFreq)) Then
+                        AnaTxCTCIndex = Array.IndexOf(ctcss_tones, Double.Parse(rToneFreq))
+                    End If
+
+                    If ctcss_tones.Contains(Double.Parse(cToneFreq)) Then
+                        AnaRxCTCIndex = Array.IndexOf(ctcss_tones, Double.Parse(cToneFreq))
+                    End If
+
+                    ' Calculate values for <AnaTxCTCFlag> <AnaRxCTCFlag>
+                    Dim AnaTxCTCFlag As Integer = 0
+                    Dim AnaRxCTCFlag As Integer = 0
+
+                    If Tone = "" Then
+                        AnaTxCTCFlag = 0
+                        AnaRxCTCFlag = 0
+                    ElseIf Tone = "Tone" Then
+                        AnaTxCTCFlag = 1
+                        AnaRxCTCFlag = 0
+                    ElseIf Tone = "TSQL" Then
+                        AnaTxCTCFlag = 1
+                        AnaRxCTCFlag = 1
+                    ElseIf Tone = "Cross" AndAlso CrossMode = "->Tone" Then
+                        AnaTxCTCFlag = 0
+                        AnaRxCTCFlag = 1
+                    ElseIf Tone = "Cross" AndAlso CrossMode = "Tone->Tone" Then
+                        AnaTxCTCFlag = 1
+                        AnaRxCTCFlag = 1
+                    End If
+
+                    ' Calculate <TxPowerLevel>
+                    Dim TxPowerLevel As Integer = 2 ' Default to high
+                    If Power = "4.0W" Then ' Chirp config for a BF=8HP uses these values
+                        TxPowerLevel = 1
+                    ElseIf Power = "1.0W" Then
+                        TxPowerLevel = 0
+                    End If
+
+                    ' Calculate the <BandWidth>
+                    Dim Bandwidth As Integer = 0 ' Default to 25kHz
+                    If Mode = "NFM" Then
+                        Bandwidth = 1
+                    End If
+
+                    ' Process the row data
+                    ' ...
+
+                    Using output_file As New StreamWriter(targetfile, True)
+                        output_file.WriteLine("    <Channel Name=""" & Name & """ chanIndex=""" & chanIndex & """>")
+                        output_file.WriteLine("      <BandWidth>" & Bandwidth & "</BandWidth>")
+                        output_file.WriteLine("      <TxFreq>" & TxFreq & "</TxFreq>")
+                        output_file.WriteLine("      <RxFreq>" & RxFreq & "</RxFreq>")
+                        output_file.WriteLine("      <TxPowerLevel>" & TxPowerLevel & "</TxPowerLevel>")
+                        output_file.WriteLine("      <AnaTxCTCFlag>" & AnaTxCTCFlag & "</AnaTxCTCFlag>")
+                        output_file.WriteLine("      <AnaRxCTCFlag>" & AnaRxCTCFlag & "</AnaRxCTCFlag>")
+                        output_file.WriteLine("      <AnaTxCTCIndex>" & AnaTxCTCIndex & "</AnaTxCTCIndex>")
+                        output_file.WriteLine("      <AnaRxCTCIndex>" & AnaRxCTCIndex & "</AnaRxCTCIndex>")
+                        output_file.WriteLine("      <FreqStep>2</FreqStep>")
+                        output_file.WriteLine("      <FreqReverseFlag>0</FreqReverseFlag>")
+                        output_file.WriteLine("      <EncryptFlag>0</EncryptFlag>")
+                        output_file.WriteLine("      <BusyNoTx>0</BusyNoTx>")
+                        output_file.WriteLine("      <PTTIdFlag>0</PTTIdFlag>")
+                        output_file.WriteLine("      <DTMFDecode>0</DTMFDecode>")
+                        output_file.WriteLine("      <AMChanFlag>0</AMChanFlag>")
+                        output_file.WriteLine("    </Channel>")
+                    End Using
+
+                    ListBox1.Items.Add("Converted: " & chanIndex & " " & Name)
+
+                End While
+            End Using
+
+            ' Read all lines from the input file
+            Dim lines As String() = File.ReadAllLines(cxffile)
+
+            ' Initialize a flag variable to False
+            Dim found_keyword As Boolean = False
+
+            ' Create a list to store the lines to be appended
+            Dim linesToAppend As List(Of String) = New List(Of String)()
+
+            ' Loop through the lines
+            For Each line As String In lines
+                ' Check if the line contains the keyword
+                If line.Contains("</Channels_MR>") Then
+                    ' Set the flag variable to True to start copying lines
+                    found_keyword = True
+                End If
+
+                ' If the flag variable is True, add the line to the list
+                If found_keyword Then
+                    linesToAppend.Add(line)
+                End If
+            Next
+
+            ' Append the lines to the output file
+            File.AppendAllLines(targetfile, linesToAppend)
+
+            lblResult.Text = "Output written to: " & targetfile
+
+        Else
+            lblResult.Text = "Specify both files to proceede"
+        End If
+
+    End Sub
+
+
+End Class
